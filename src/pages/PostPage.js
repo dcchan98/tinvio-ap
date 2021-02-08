940c33854edcbcbb5f011c74defdfdcfa24da79d
Sean Chan <dcchan98@gmail.com>
2021-02-08 15:09:16 +0800

    add fetching functionality

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -1,51 +1,75 @@
-import React, { Component } from 'react'
+/** @format */
 
-import HeaderImage from '../components/HeaderImage'
-import IconText from '../components/IconText'
-import Card from '../components/Card'
+import React, { Component } from "react";
 
-const imageURL = "images/image.jpg"
-const phoneURL = 'images/phone.png'
-const categoryURL = 'images/category.png'
-const shopURL = 'images/shop.png'
+import { connect } from "react-redux";
+import { fetchUsers } from "../redux/user/user-actions";
 
+import HeaderImage from "../components/HeaderImage";
+import IconText from "../components/IconText";
+import Card from "../components/Card";
 
-export default class PostPage extends Component {
-  render() {
-    return (
-      <div>
-      <HeaderImage imageURL = {imageURL}/>
+const imageURL = "images/image.jpg";
+const phoneURL = "images/phone.png";
+const categoryURL = "images/category.png";
+const shopURL = "images/shop.png";
 
-      <br></br>
-      <div className="container">
-      <h1>Leanne Graham</h1>
-      <br/>
-      <IconText iconsURL = {phoneURL} text = {'12345'}/>
-      <br/>
-      <IconText iconsURL = {categoryURL} text = {'Harness . Real Time . Market'}/>
-      <br/>
-      <IconText iconsURL = {shopURL} text = {'Kulas Light, Apt. 556 , Singapore 123456'}/>
-      <br/>
-      <br/>
-      <h1>Leanne Graham Post's</h1>
-      <br/>
-      <h6 className="text-secondary">2 POSTS</h6>
-    {/* Use map to change  */}
-      <br/>
-      <Card title ="Title1" body ="Body1"/>
-      <br/>
-      <Card title ="Title2" body ="Body2"/>
+class PostPage extends Component {
+	constructor(props) {
+		super(props);
+	}
+	componentDidMount() {
+		this.props.fetchUsers();
+	}
 
-    
+	render() {
+		return (
+			<div>
+				<HeaderImage imageURL={imageURL} />
+				<br></br>
+				<div className='container'>
+					<h1>Users</h1>
 
-      </div>
+					{this.props.fetchedUsers != null ?
 
-      
-     
+           this.props.fetchedUsers.map(user=>user.name)
 
-      
-        
-      </div>
-    )
-  }
+           : ""}
+
+					<h1>Leanne Graham</h1>
+					<br />
+					<IconText iconsURL={phoneURL} text={"12345"} />
+					<br />
+					<IconText
+						iconsURL={categoryURL}
+						text={"Harness . Real Time . Market"}
+					/>
+					<br />
+					<IconText
+						iconsURL={shopURL}
+						text={"Kulas Light, Apt. 556 , Singapore 123456"}
+					/>
+					<br />
+					<br />
+					<h1>Leanne Graham Post's</h1>
+					<br />
+					<h6 className='text-secondary'>2 POSTS</h6>
+					{/* Use map to change  */}
+					<br />
+					<Card title='Title1' body='Body1' />
+					<br />
+					<Card title='Title2' body='Body2' />
+				</div>
+			</div>
+		);
+	}
 }
+
+const mapStateToProps = (state /*, ownProps*/) => {
+	return {
+		fetchedUsers: state.user.fetchedUsers,
+	};
+};
+const mapDispatchToProps = { fetchUsers };
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostPage);
--- src/redux/user/user-actions.js
+++ src/redux/user/user-actions.js
@@ -1,10 +1,8 @@
 export const fetchUsers = user =>{
 
-
   return async function(dispatch){
     const res = await fetch("https://jsonplaceholder.typicode.com/users")
     const resJSON = await(res.json())
-    dispatch({ type: 'FETCH_POSTS', payload: resJSON });
-  
+    dispatch({ type: 'FETCH_USERS', payload: resJSON });
   }
 }
