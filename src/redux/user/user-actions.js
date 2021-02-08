f84f1f772703b3f2b2d151351e36f279debd40b6
Sean Chan <dcchan98@gmail.com>
2021-02-08 14:40:32 +0800

    create async action creator

--- src/redux/user/user-actions.js
+++ src/redux/user/user-actions.js
@@ -1,6 +1,10 @@
-export const setCurrentUser = user =>{
-  return {
-    type: 'FETCH_USERS',
-    payload: user  
+export const fetchUsers = user =>{
+
+
+  return async function(dispatch){
+    const res = await fetch("https://jsonplaceholder.typicode.com/users")
+    const resJSON = await(res.json())
+    dispatch({ type: 'FETCH_POSTS', payload: resJSON });
+  
   }
 }
