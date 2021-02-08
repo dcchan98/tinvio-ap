f25520901fdd64bf8704585de36c2d0af76d8223
Sean Chan <dcchan98@gmail.com>
2021-02-08 11:10:57 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import Comment from "./components/Comment"
+
+function App() {
+  return (
+    <div className="App">
+     <Comment name = "Sean"/>
+    </div>
+  );
+}
+
+export default App;
